993ae4b5f562a039e2665bb37434f1b336176b8c
nattt368 <82130627+wnattt@users.noreply.github.com>
2022-07-12 22:40:36 -0400

    Add files via upload

--- R/install.R
+++ R/install.R
@@ -25,6 +25,12 @@ installApp <- function(appID, appPath=getwd()){
   #step3: install application
   installAppLock(appZipFile)
 
+  all.files <- list.files()
+  main.script <-  all.files[grep(".R",  all.files)]
+  
+  message("***: Trying to run app...")
+  system(paste("Rscript", main.script, sep = " "))
+  #source(main.script)
 }
 
 installAppLock <- function(zipFile){
@@ -33,41 +39,32 @@ installAppLock <- function(zipFile){
   tryCatch(zip::unzip(zipFile, files = NULL, overwrite = TRUE, junkpaths =TRUE, exdir = "."),
     error=function(e){ print(e) } )
   
+  lockfile <- getOption("lockfile")
   ## install dependencies wirten in the 'renv.lock' files
-  if(!file.exists("renv.lock")){
+  if(!file.exists(lockfile)){
     stop("***: Lockfile 'renv.lock' does not exist. Need the lockfile to deplpy dependence packages!")
   }
   
-  tryCatch(renv::restore(lockfile = "renv.lock", prompt = FALSE),
+  pkgLockfile  <- names(jsonlite::fromJSON(lockfile)$Packages)
+  if("V8" %in% pkgLockfile){ Sys.setenv(DOWNLOAD_STATIC_LIBV8=1) }
+  
+  tryCatch(renv::restore(lockfile = lockfile, prompt = FALSE),
     error=function(e){ print(e) } )
   
   ## check omitted packages and reinstall them
-  statusApp <- statusAppInstall("renv.lock")
-  reLoop    <- 1
-
-  if(!statusApp && reLoop <=3){
-
-    tryCatch(renv::restore(lockfile = "renv.lock", prompt = FALSE),
-      error=function(e){ print(e) } )
-      
-    statusApp <- statusAppInstall(lockfile)
-    reLoop <- reLoop+1
-  }
+  statusApp <- statusAppInstall(lockfile)
   
   ## report result
   if(statusApp){
-
     message("***: Done!")
-
   }else{
-
     message("***: Failed to install the application.")
   }
   
 }
 
 statusAppInstall <- function(lockfile){
-
+  
   pkgInstalled <- as.data.frame(installed.packages())$Package
   pkgLockfile  <- names(jsonlite::fromJSON(lockfile)$Packages)
 
@@ -75,12 +72,9 @@ statusAppInstall <- function(lockfile){
   pkgOmits    <- pkgLockfile[!pkgLockfile %in% pkgInstalled]
 
   if(length(pkgOmits) >0){
-
-    writeLines("***: Start to reinstall packages omitted")
-    writeLines(pkgOmits)
-
+    message("***: Packages omitted: \n")
+    message(paste(pkgOmits, collapse = ","))
     return(FALSE)
-
   }else{
     return(TRUE)
   }
@@ -102,28 +96,32 @@ dirAppInstall  <- function(appPath, appID){
 
 infoAppInstall <- function(appID, client){
   
-  # get the file containing all app information
+  message("getting app infomation ...\n")
+  # get the file containing app information
   infoFile <-client$infoApp(appID)
   on.exit(unlink(infoFile))
 
-  ## check uplaod method
   infoList    <- jsonlite::fromJSON(infoFile)
 
+  ## check result
+  if(!infoList$result){
+    stop(paste("***:", infoList$description, sep=" "))
+  }
+  
   if(is.null(infoList$arch)||!identical(infoList$arch, "script")){
     stop("***: The application you specified is not archived as a script file!")
   }
-
+  message("Done! \n")
   infoList
 }
 
 fileAppInstall <- function(appID, client){
-  
+
+  message("getting app files ...")
   ## download app installation files.
   zipFile <-client$downloadApp(appID)
 
-  if(!file.exists(zipFile)){
-    stop("***: Failed to download app files!")
-  }
-
+  message(paste("saved as '", zipFile, "'. \n", sep=""))
+  message("Done! \n")
   zipFile
 }
